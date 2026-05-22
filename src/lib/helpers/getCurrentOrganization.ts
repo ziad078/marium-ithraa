7e39d48bd9cdcaaa6d391cc1b2002875feba351e
ziad078 <ziadzayd79@gmail.com>
2026-05-22 12:47:15 +0300

    console.log

--- src/lib/helpers/getCurrentOrganization.ts
+++ src/lib/helpers/getCurrentOrganization.ts
@@ -3,6 +3,9 @@ import nextAuthOptions from "@/server/auth"
 import { getServerSession } from "next-auth"
 
 export const getCurrentOrganization = async () => {
+  console.log("i'am the error")
     const session = await getServerSession(nextAuthOptions)
+  console.log("i'am the error33")
+
     return getUserOrganization(session?.user?.id||"")
   }

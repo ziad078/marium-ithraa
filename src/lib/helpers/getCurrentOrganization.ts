d589a4f991a1f5ae5c31dd39db3abc5a4f393ed0
ziad078 <ziadzayd79@gmail.com>
2026-05-23 10:33:38 +0300

    starting refactor

--- src/lib/helpers/getCurrentOrganization.ts
+++ src/lib/helpers/getCurrentOrganization.ts
@@ -4,7 +4,5 @@ import { getServerSession } from "next-auth"
 
 export const getCurrentOrganization = async () => {
     const session = await getServerSession(nextAuthOptions)
-    console.log("session .user>>>",session?.user)
-    console.log("get org=>>>",await getUserOrganization(session?.user?.id||""))
     return getUserOrganization(session?.user?.id||"")
   }
